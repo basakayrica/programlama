4c0eefa0cec4671471faa3b8b450a40a2f65b327
basakayrica <basakayr1cahnk@gmail.com>
2025-03-28 09:17:44 +0300

    Add files via upload

---
+++ 250307/ornek2.py
@@ -0,0 +1,4 @@
+sayilar=[35,26,81,64]
+#b)listeyi listeyi tersten yazdırınız
+sayilar.reverse()
+print(sayilar)
---
+++ 250314/ornek2.py
@@ -0,0 +1,6 @@
+adi=input("adınızı giriniz: ")
+şifre=input("şifrenizi giriniz:")
+if adi=="türkiye" and şifre=="1923":
+  print("giriş başarılı ")
+else:
+   print("kullanıcı adı yada şifre yanlış")
